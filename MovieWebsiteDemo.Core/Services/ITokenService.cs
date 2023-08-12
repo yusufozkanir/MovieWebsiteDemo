497977a407929c2681abad33ef881b79adb7526f
Yusuf Özkan İr <yusufozkanir@yaani.com>
2023-08-12 20:22:55 +0300

    Added TokenDto and ITokenService

---
+++ MovieWebsiteDemo.Core/Services/ITokenService.cs
@@ -0,0 +1,18 @@
+﻿using MovieWebsiteDemo.Core.Configurations;
+using MovieWebsiteDemo.Core.DTOs;
+using MovieWebsiteDemo.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieWebsiteDemo.Core.Services
+{
+    public interface ITokenService
+    {
+        TokenDto CreateToken(UserApp userApp);
+
+        ClientTokenDto CreateTokenByClient(Client client);
+    }
+}
