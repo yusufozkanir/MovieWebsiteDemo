9cf13846edab19acbd783a35f4a9969bc50c537b
Yusuf Özkan İr <yusufozkanir@yaani.com>
2023-08-14 14:42:06 +0300

    Added UserService

---
+++ MovieWebsiteDemo.Service.Business/Services/UserService.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using AutoMapper.Internal.Mappers;
+using Azure;
+using Microsoft.AspNetCore.Identity;
+using MovieWebsiteDemo.Core.DTOs;
+using MovieWebsiteDemo.Core.Models;
+using MovieWebsiteDemo.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieWebsiteDemo.Service.Business.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly UserManager<UserApp> _userManager;
+        private readonly IMapper _mapper;
+
+        public UserService(UserManager<UserApp> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<CustomResponseDto<UserAppDto>> CreateUserAsync(CreateUserDto createUserDto)
+        {
+            var user = new UserApp { Email = createUserDto.Email, UserName = createUserDto.UserName };
+
+            var result = await _userManager.CreateAsync(user, createUserDto.Password);
+
+            if (result.Succeeded)
+            {
+                var errors = result.Errors.Select(x => x.Description).ToList();
+
+                return CustomResponseDto<UserAppDto>.Fail(400, new ErrorDto(errors, true));
+            }
+
+            return CustomResponseDto<UserAppDto>.Success(200, _mapper.Map<UserAppDto>(user));
+        }
+
+        public async Task<CustomResponseDto<UserAppDto>> GetUserByNameAsync(string userName)
+        {
+            var user = await _userManager.FindByNameAsync(userName);
+
+            if (user == null)
+            {
+                return CustomResponseDto<UserAppDto>.Fail(404, "UserName not found", true);
+            }
+            return CustomResponseDto<UserAppDto>.Success(200, _mapper.Map<UserAppDto>(user));
+        }
+    }
+}
