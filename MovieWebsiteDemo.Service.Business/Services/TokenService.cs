24b9b76a5629da28e1f5c3eeab1f01f36407f12d
Yusuf Özkan İr <yusufozkanir@yaani.com>
2023-08-14 09:23:44 +0300

    TokenService created

---
+++ MovieWebsiteDemo.Service.Business/Services/TokenService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using MovieWebsiteDemo.Core.Configurations;
+using MovieWebsiteDemo.Core.DTOs;
+using MovieWebsiteDemo.Core.Models;
+using MovieWebsiteDemo.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieWebsiteDemo.Service.Business.Services
+{
+    public class TokenService : ITokenService
+    {
+        private readonly UserManager<UserApp> _userManager;
+        private readonly CustomTokenOption _tokenOption;
+
+        public TokenService(UserManager<UserApp> userManager, IOptions<CustomTokenOption> options)
+        {
+            _userManager = userManager;
+            _tokenOption = options.Value;
+        }
+
+        private string CreateRefreshToken()
+        {
+            var numberByte = new byte[32];
+            using var rnd = RandomNumberGenerator.Create();
+            rnd.GetBytes(numberByte);
+            return Convert.ToBase64String(numberByte);
+        }
+
+        public TokenDto CreateToken(UserApp userApp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ClientTokenDto CreateTokenByClient(Client client)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
