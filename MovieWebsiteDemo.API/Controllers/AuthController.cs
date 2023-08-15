d2f6d53296101463aab19ddb293d8ba326d7cd0c
Yusuf Özkan İr <yusufozkanir@yaani.com>
2023-08-15 14:46:16 +0300

    Program.cs edited

--- MovieWebsiteDemo.API/Controllers/AuthController.cs
+++ MovieWebsiteDemo.API/Controllers/AuthController.cs
@@ -19,32 +19,25 @@ namespace MovieWebsiteDemo.API.Controllers
         [HttpPost]
         public async Task<IActionResult> CreateToken(LoginDto loginDto)
         {
-            var result = await _authenticationService.CreateTokenAsync(loginDto);
-
-            return CreateActionResult(result);
+            return CreateActionResult(await _authenticationService.CreateTokenAsync(loginDto));
         }
 
         [HttpPost]
         public IActionResult CreateTokenByClient(ClientLoginDto clientLoginDto)
         {
-            var result = _authenticationService.CreateTokenByClient(clientLoginDto);
-
-            return CreateActionResult(result);
+            return CreateActionResult(_authenticationService.CreateTokenByClient(clientLoginDto));
         }
 
         [HttpPost]
         public async Task<IActionResult> RevokeRefreshToken(RefreshTokenDto refreshTokenDto)
         {
-            var result = await _authenticationService.RevokeRefreshToken(refreshTokenDto.Token);
-
-            return CreateActionResult(result);
+            return CreateActionResult(await _authenticationService.RevokeRefreshToken(refreshTokenDto.Token));
         }
 
         [HttpPost]
         public async Task<IActionResult> CreateTokenByRefreshToken(RefreshTokenDto refreshTokenDto)
         {
-            var result = await _authenticationService.CreateTokenByRefreshToken(refreshTokenDto.Token);
-            return CreateActionResult(result);
+            return CreateActionResult(await _authenticationService.CreateTokenByRefreshToken(refreshTokenDto.Token));
         }
     }
 }
